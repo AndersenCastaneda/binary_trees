ca495c8521530ebfff50a6bcd49b572b39918def
AndersenCastaneda <andersenpiano@gmail.com>
2020-07-21 11:45:30 +0000

    Add task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,26 +7,8 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-
 	if (!tree)
 		return (0);
 
-	/**
-	*	//Whit recursion
-	*	if (tree->parent)
-	*		depth = 1 + binary_tree_depth(tree->parent);
-	*
-	*	//Whit recursion and ternary operator
-	*	depth = (tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0;
-	*/
-
-	/*Normal Loop*/
-	while (tree->parent)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-
-	return (depth);
+	return ((tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
