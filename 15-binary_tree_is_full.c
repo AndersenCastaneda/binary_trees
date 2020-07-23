fcebcc092d174c9c8aa3b78a5330b0a8267a2295
AndersenCastaneda <andersenpiano@gmail.com>
2020-07-23 04:20:44 +0000

    Fix Betty Style

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -10,5 +10,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	return ((binary_tree_is_full(tree->left) == binary_tree_is_full(tree->right)) ? 1 : 0);
+	return ((binary_tree_is_full(tree->left) == binary_tree_is_full(tree->right))
+		? 1 : 0);
 }
